26f1e7f008a1515164a4e2a0bcd7b96843f0549e
Руслан Давлетбаев <inferion00@gmail.com>
2021-09-17 12:45:25 +0300

    added fonts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,11 @@
 import { createApp } from 'vue';
+import WebFont from 'webfontloader';
+
 import App from './App.vue';
 import store from './store';
 
 import './assets/styles/global.scss';
+
+WebFont.load({ custom: { families: ['Montserrat'] } });
 
 createApp(App).use(store).mount('#app');
