194f345a9bc760faa5f73befb9093c3808064a88
Руслан Давлетбаев <inferion00@gmail.com>
2021-09-17 23:09:27 +0300

    added icons

---
+++ src/components/Icon.vue
@@ -0,0 +1,31 @@
+<template>
+  <svg :class="['icon', className]">
+    <use :href="'/sprite.svg#' + icon" />
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Icon',
+  props: {
+    className: String,
+    icon: {
+      type: String,
+      validator(value: string) {
+        return ['clock', 'arrow-left', 'arrow-right'].indexOf(value) !== -1;
+      },
+      required: true,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  display: block;
+  width: 1em;
+  height: 1em;
+}
+</style>
