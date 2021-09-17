77c9690d5a488dd1e7725ff266e3fb6b55640469
Руслан Давлетбаев <inferion00@gmail.com>
2021-09-17 13:59:14 +0300

    setup store

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,13 @@
 import { createApp } from 'vue';
 import WebFont from 'webfontloader';
 
-import App from './App.vue';
+import AppComponent from './App.vue';
 import store from './store';
 
 import './assets/styles/global.scss';
 
 WebFont.load({ custom: { families: ['Montserrat'] } });
 
-createApp(App).use(store).mount('#app');
+export const App = createApp(AppComponent).use(store);
+
+App.mount('#app');
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,25 @@
 import { createStore } from 'vuex';
 
+import { App } from '@/main';
+
 export default createStore({
   state: {
+    episodes: [],
   },
   mutations: {
+    receivedEpisodes(state, episodes) {
+      state.episodes = episodes;
+    }
   },
   actions: {
-  },
-  modules: {
+    async getEpisodes({ commit }, eventId: string) {
+      const episodes = await App.config.globalProperties.$api.timetable.getEpisodes();
+
+      if (!episodes) return null;
+
+      commit('receivedEpisodes', episodes);
+
+      return episodes;
+    }
   },
 });
